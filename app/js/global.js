127baab8369a437923d0b1966e6e69b0c155c817
Hedley Smith <hedley@agilecollective.com>
2014-09-15 10:24:14 +0100

    Finishin off content, final QA

--- app/js/global.js
+++ app/js/global.js
@@ -8,6 +8,8 @@ $(document).ready(function(){
   $( ".timeline-list > li" ).click(function() {
     $( this ).toggleClass( "active" );
     $( this ).children("div").toggleClass( "hidden" );
+  }).children("div").click(function(e) {
+    e.stopPropagation(); // Stop child div from firing click event.
   });
 });
 
